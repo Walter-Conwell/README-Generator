768802b8c69c3643a04f63ec093fa6ebf5adf771
Walter-Conwell <131832544+Walter-Conwell@users.noreply.github.com>
2023-08-04 16:49:41 -0600

    fixed file structure and repository problem

---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,38 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+//psuedocode if
+// check what blicense is passed and return badge.
+
+else {
+  return "",
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+//psuedo code if
+//if badge is recognized, return license link.
+
+else {
+  return "",
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+//psuedocode
+//populate license section.
+else {
+  return "",
+}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  //psuedocode
+  //license (badge), user input, ******JSON info?????
+  return `# ${data.title}
+
+`;
+}
+
+module.exports = generateMarkdown;
